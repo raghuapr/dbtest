27f9f5e3450ea4bbf793758c53ea66573d1c19a7
raghuapr <47568274+raghuapr@users.noreply.github.com>
2020-10-30 15:43:15 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,9 @@
   
 <h2> Atos Managed OpenShift </h2>
 
-<h2> Shared Platform DevOps, Tools and Processes </h2>
+<h1> OpenShift Container Platform installation overview </h1>
+
+<h2> The OpenShift Container Platform installation program offers you flexibility. You can use the installation program to deploy a cluster on infrastructure that the installation program provisions and the cluster maintains or deploy a cluster on infrastructure that you prepare and maintain.  </h2>
 
 <h2> Welcome to OpenShift Version 4.5 !! </h2>
 
