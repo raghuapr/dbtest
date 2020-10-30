cfed9c6aee9a234e980cf249db845b406dbe73f6
raghuapr <47568274+raghuapr@users.noreply.github.com>
2020-10-30 13:05:11 +0530

    Update index.php

--- index.php
+++ index.php
@@ -4,13 +4,11 @@
 
 <h1> AMOS </h1>
   
-<h1> Hello this is Raghavendra AP </h1>
+<h2> Atos Managed OpenShift </h2>
 
-<h1> Welcome to OpenShift Version3.11 !! </h1>
+<h2> Welcome to OpenShift Version 4.5 !! </h2>
 
-<h1> This is only for testing purpose </h1>
-
-<h1> Try /dbtest.php extension :-) </h1>
+<h2> This is only for testing purpose </h2>
 
 
 </body>
