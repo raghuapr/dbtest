04f4a3b4b342404364bfdf28bbed4ae7493f5c3f
raghuapr <47568274+raghuapr@users.noreply.github.com>
2020-10-30 15:48:49 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,11 +6,9 @@
   
 <h2> Atos Managed OpenShift </h2>
 
-<h1> OpenShift Container Platform installation overview </h1>
-
-<h2> The OpenShift Container Platform installation program offers you flexibility. You can use the installation program to deploy a cluster on infrastructure that the installation program provisions and the cluster maintains or deploy a cluster on infrastructure that you prepare and maintain.  </h2>
-
 <h2> Welcome to OpenShift Version 4.5 !! </h2>
+
+<h2> Red Hat OpenShift Container Platform provides developers and IT organizations with a hybrid cloud application platform for deploying both new and existing applications on secure, scalable resources with minimal configuration and management overhead. OpenShift Container Platform supports a wide selection of programming languages and frameworks, such as Java, JavaScript, Python, Ruby, and PHP. </h2>
 
 <h2> This is only for testing purpose </h2>
 
