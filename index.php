e285c9319545912b42d41574e1a6f0f46ec19086
raghuapr <47568274+raghuapr@users.noreply.github.com>
2020-10-30 14:57:24 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,6 +6,8 @@
   
 <h2> Atos Managed OpenShift </h2>
 
+<h2> Shared Platform DevOps, Tools and Processes </h2>
+
 <h2> Welcome to OpenShift Version 4.5 !! </h2>
 
 <h2> This is only for testing purpose </h2>
