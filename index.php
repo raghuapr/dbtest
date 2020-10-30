34691ba998efd34d83ed450876ed7dc58742264c
raghuapr <47568274+raghuapr@users.noreply.github.com>
2020-10-30 17:37:39 +0530

    Update index.php

--- index.php
+++ index.php
@@ -8,8 +8,6 @@
 
 <h2> Welcome to OpenShift Version 4.5 !! </h2>
 
-<h2> Red Hat OpenShift Container Platform provides developers and IT organizations with a hybrid cloud application platform for deploying both new and existing applications on secure, scalable resources with minimal configuration and management overhead. OpenShift Container Platform supports a wide selection of programming languages and frameworks, such as Java, JavaScript, Python, Ruby, and PHP. </h2>
-
 <h2> This is only for testing purpose </h2>
 
 
